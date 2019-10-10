c6af90bc2d271efa459212a2f71628f733ff87ec
laportenier <lilly.portenier@gmail.com>
2019-10-10 18:39:56 -0400

    trying ot finx my dang timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,66 +3,71 @@ $(document).ready(function(){
 
 
 
-var count = 20;
-var current = 0;
-var correct = 0;
-var incorrect = 0;
-var unanswered = 0;
+    var count = 10;
+    var current = 0;
+    var correct = 0;
+    var incorrect = 0;
+    var unanswered = 0;
+    var userAnswer;
+    var intervalTimer;
+    var button;
+    var optionsArr;
 
-var QS = [{
-    question: "The part of an atom with a negative electrical charge is the?",
-    options: ["Nutron", "Electron", "Proton", "Atomic Nucleus"],
-    answer: 1
-}, {
-    question: "What is the fastest bird on foot?",
-    options: ["Ostrich", "Sparrow", "Eagle", "Dragon"],
-    answer: 0
-} , {
-    question: "What planet is closest to the sun?",
-    options: ["Venus", "Mars", "Earth", "Mercury"],
-    answer: 3
-} , {
-    question: "A heptagon is a shape with how many sides?",
-    options: ["4", "7", "8", "20"],
-    answer: 1
-} , {
-    question: "How long is one regular term of a U.S. Representative?",
-    options: ["3", "1", "2", "4"],
-    answer: 2
-} , {
-    question: "What is the largest South American country by area?",
-    options: ["Argentina", "Brazil", "Chile", "Mexico"],
-    answer: 1
-} , {
-    question: "Which of the floowing states is NOT part of the Four Corners?",
-    options: ["New Mexico", "Utah", "Colorado", "Nevada"],
-    answer: 3
-} , {
-    questoin: "'Carfeully' is an example of what type of word?", 
-    options: ["Advjective", "Noun", "Verb", "Adverb"],
-    answer: 3
-} , {
-    question: "In the Northern Hemisphere, what month is the autumnal equnox?",
-    options: ["August", "September", "October", "November"],
-    answer: 1
-} , {
-    question: "Inca civilizations were concentrated on what continent?", 
-    options: ["South America", "Africa", "North America", "Europe"],
-    answer: 0
-}
-] 
 
-$("#start").click(function() {
-    $(this).hide();
-    $('#result').hide();
-    $("#instructions").hide();
-    $("#remaining-time").show();
-    questions();
-});
-function questions() {
-    
-    if (QS[current]) {
-        $("#timer").html(count + " secs");
+
+    var QS = [{
+        question: "The part of an atom with a negative electrical charge is the?",
+        options: ["Nutron", "Electron", "Proton", "Atomic Nucleus"],
+        answer: 1
+    }, {
+        question: "What is the fastest bird on foot?",
+        options: ["Ostrich", "Sparrow", "Eagle", "Dragon"],
+        answer: 0
+    } , {
+        question: "What planet is closest to the sun?",
+        options: ["Venus", "Mars", "Earth", "Mercury"],
+        answer: 3
+    } , {
+        question: "A heptagon is a shape with how many sides?",
+        options: ["4", "7", "8", "20"],
+        answer: 1
+    } , {
+        question: "How long is one regular term of a U.S. Representative?",
+        options: ["3", "1", "2", "4"],
+        answer: 2
+    } , {
+        question: "What is the largest South American country by area?",
+        options: ["Argentina", "Brazil", "Chile", "Mexico"],
+        answer: 1
+    } , {
+        question: "Which of the floowing states is NOT part of the Four Corners?",
+        options: ["New Mexico", "Utah", "Colorado", "Nevada"],
+        answer: 3
+    } , {
+        questoin: "'Carfeully' is an example of what type of word?", 
+        options: ["Advjective", "Noun", "Verb", "Adverb"],
+        answer: 3
+    } , {
+        question: "In the Northern Hemisphere, what month is the autumnal equnox?",
+        options: ["August", "September", "October", "November"],
+        answer: 1
+    } , {
+        question: "Inca civilizations were concentrated on what continent?", 
+        options: ["South America", "Africa", "North America", "Europe"],
+        answer: 0
+    }];
+
+    $("#start").click(function() {
+        $(this).hide();
+        $('#result').hide();
+        $("#instructions").hide();
+        $("#remaining-time").show();
+        questions();
+    });
+    function questions() {
+        
+        setInterval(timer, 1000);
+
         $("#questions").html(QS[current].question);
         optionsArr = QS[current].options;
         
@@ -72,45 +77,89 @@ function questions() {
             button.attr("data-id", i);
             $("#buttons-div").append(button);
         };
+        // $("#Unanswered").html("Unanswered: " + unanswered);
+        // $("#Correct").html("Correct: " + correct);
+        // $("#Incorrect").html("Incorrect: " + incorrect);
 
-        window.trivaCounter = setInterval(timer, 1000);
-    }
-    else { 
-        $("#Unanswered").html("Unanswered: " + unanswered);
-        $("#Correct").html("Correct: " + correct);
-        $("#Incorrect").html("Incorrect: " + incorrect);
     };
-};
+    
 
-function timer() {
-    count--;
-    if (count <= 0) {
-        setTimeout(function() {
+    function timer() {
+        count--;
+        $("#timer").html(count + " seconds");
+
+        if (count <= 0) {
+            unanswered++;
             nextQuestion();
-        });
-    } 
-    else {
-        $("#timer").html(count + " secs");
-    }
-};
-function nextQuestion() {
-    current++;
-    clearInterval(window.triviaCounter);
-    count = 20;
-    $("#timer").html("00");
-   
-    setTimeout(function() {
-        reset();
-        question();
-    },5000)
-};
-function reset() {
+        } 
+        else if (userChoice === correctAnswer) {
+            $("#buttons-div").hide();
+            correctAnswer();
+        }
+        else {
+            $("#buttons-div").hide();
+            wrongAnswer();
+        }
+    };
 
-    $("#Unanswered").html("Unanswered: " + unanswered);
-    $("#Correct").html("Correct: " + correct);
-    $("#Incorrect").html("Incorrect: " + incorrect);
+    function correctAnswer() {
+        correct++;
+        nextQuesion();
+    };
 
-};
+    function wrongAnswer() {
+        // $("#buttons-div").hide();
+        incorrect++;
+        nextQuestion();
+    };
+
+    function nextQuestion() {
+        current++;
+        count = 10;
+        questions();
+
+        // setTimeout(function() {
+        //     // reset();
+        //     questions();
+        // },1000)
+
+    };
+    // reset fix needed
+    // function reset() {
+    //     // $('div[id]').each(function(item) {
+    //     //     $(this).html('');
+    //     // });
+    //     $("#Unanswered").html("Unanswered: " + unanswered);
+    //     $("#Correct").html("Correct: " + correct);
+    //     $("#Incorrect").html("Incorrect: " + incorrect);
+
+    // };
+    // function answer() {
+        
+
+    // };
+
+    $("#buttons-div").on("click", "button", function(){
+        
+        userChoice = $(this).data("id");
+        index = QS[current].answer;
+        correct = QS[current].options[index];
+
+    //     if (userChoice === index) {
+    //         $("#resluts-div").text("Wrong answer!");
+    //         // answer(false);
+    //     }
+    //     else {
+    //         $("#resluts-div").text("Correct!!");
+    //         // answer(true);
+    //     }
+    //     nextQuestion();
+        console.log(userChoice);
+    
+    // // answer function work needed
+
+    });
+
 });
 
 // start the timer, hide the start page, show first question 
